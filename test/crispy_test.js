ac41ff3c1a01c9e5310dbe13bb5a3ae50ad5e930
George Anderson <guimaraes.george@gmail.com>
2013-12-15 19:23:38 -0200

    Added effort logging

--- test/crispy_test.js
+++ test/crispy_test.js
@@ -6,24 +6,33 @@ var assert = require('assert')
 	, crispy = require('../lib/crispy.js');
 
 describe('Project', function() {
-	var cp;
+	var cp = new crispy.Project();
 
 	beforeEach(function() {
 		cp = new crispy.Project();
 	})
 
-	describe('New Project', function() {
+	describe('Default New Project', function() {
 		it('should have a name', function() {
 			assert.equal('New Project', cp.name);
 		})
-		it('should have estimated time', function() {
-			cp.estimated = 200;
-			assert.equal(200, cp.estimated);
+		it('should have 60 minutes of effort', function() {
+			assert.equal(60, cp.remaining());
 		})
-		it('should calculate remaining time', function() {
-			cp.estimated = 200;
-			cp.actual = 100;
-			assert.equal(100, cp.remaining());
+		it('should update effort after time is logged', function() {
+			cp.log(10);
+			assert.equal(50, cp.remaining());
+		})
+		it('should update effort after successive time is logged', function() {
+			cp.log(10);
+			cp.log(10);
+			assert.equal(40, cp.remaining());
+		})
+		it('should update effort after time is elapsed', function(done) {
+			cp.start();
+			setTimeout(cp.stop(), 1000);
+			done();
+			assert.equal(59, cp.remaining());
 		})
 	})
 })
